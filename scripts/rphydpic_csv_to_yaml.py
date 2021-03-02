5adb493797965dc6ddc3a7a3535dc47e108977df
Eric koons <ekoons5159@gmail.com>
2021-03-02 10:28:48 -0500

    Update rphydpic_csv_to_yaml.py

--- scripts/rphydpic_csv_to_yaml.py
+++ scripts/rphydpic_csv_to_yaml.py
@@ -9,8 +9,10 @@ with open(sys.argv[1] + '.yml', "w") as my_output_file:
           next(csvfile, None)  # skip the header
           my_output_file.write("csv_data: " + '\n')
           for row in csv.reader(csvfile):
-                    my_output_file.write(" - prefix: \'" +(row[0])+ "\'\n")
-                    my_output_file.write("   description: \'" +(row[4])+" "+(row[5])+ "\'\n")
-                    my_output_file.write("   interface: \'" +(row[5])+ "\'\n")
-                    my_output_file.write("   device: \'" +(row[4])+ "\'\n")
+                    my_output_file.write(" - subnet: \'" +(row[0])+ "\'\n")
+                    my_output_file.write("   DPICint: \'" +(row[1])+ "\'\n")
+                    my_output_file.write("   vrf: \'" +(row[2])+ "\'\n")
+                    my_output_file.write("   cinint \'" +(row[3])+ "\'\n")
+                    my_output_file.write("   primarydevice: \'" +(row[4])+ "\'\n")
+                    my_output_file.write("   backupdevice: \'" +(row[4])+ "\'\n")
           my_output_file.close()
